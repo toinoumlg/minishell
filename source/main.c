35964af6f7cd026c2bf01ccfc2dac13c813291cf
Antoine Malanguerray <amalangu@z3r9p5.42lyon.fr>
2025-09-28 15:14:05 +0200

    done ?

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: amalangu <amalangu@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 12:16:39 by amalangu          #+#    #+#             */
-/*   Updated: 2025/09/28 15:10:38 by amalangu         ###   ########.fr       */
+/*   Updated: 2025/09/28 15:13:01 by amalangu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,33 +49,16 @@ void	wait_for_childrens(t_minishell *minishell)
 	while (i < minishell->size)
 		waitpid(minishell->pids[i++], &status, 0);
 	set_signals();
-	if (g_heredoc_interrupted == 0)
-		handle_child_exit_status(minishell, status);
+	handle_child_exit_status(minishell, status);
 	if (minishell->pids)
 		free(minishell->pids);
 	minishell->pids = NULL;
-}
-
-static void	handle_signal_status(t_minishell *minishell)
-{
-	if (g_heredoc_interrupted == 2)
-	{
-		minishell->last_status = 130;
-		g_heredoc_interrupted = 0;
-	}
-	if (g_heredoc_interrupted == 1)
-	{
-		minishell->last_status = 130;
-		g_heredoc_interrupted = 0;
-	}
-	return ;
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	t_minishell	minishell;
 
-	rl_catch_signals = 0;
 	if (!isatty(0) || !isatty(1) || !isatty(2))
 		return (1);
 	set_envp(&minishell, envp);
@@ -83,15 +66,8 @@ int	main(int argc, char **argv, char **envp)
 	while (argv && argc)
 	{
 		minishell.read_line = readline("minishell> ");
-		handle_signal_status(&minishell);
-		if (!minishell.read_line)
-		{
-			write(1, "exit\n", 5);
-			break ;
-		}
 		parse_read_line(&minishell);
 		exec(&minishell);
 		wait_for_childrens(&minishell);
-		handle_signal_status(&minishell);
 	}
 }
